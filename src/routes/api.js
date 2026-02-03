b032fd858efe2754ce584dda66b00e03df4957f9
JungleTea <nguyenkhanhlam91nvt@gmail.com>
2026-02-03 09:34:19 +0700

    test swagger trên deploy_URL

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,17 +1,72 @@
 const express = require("express");
 const router = express.Router();
-
+const pool = require("../config/database");
 const { requireAuth } = require("../middleware/requireAuth");
 const authController = require("../controllers/authController");
 const productController = require("../controllers/productController");
 const orderController = require("../controllers/orderController");
-const dashboardController = require("../controllers/franchiseStaff_dashboardController");
+const franchiseStaff_dashboardController = require("../controllers/franchiseStaff_dashboardController");
+
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Login
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [email, password]
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "storestaff1@moon.vn"
+ *               password:
+ *                 type: string
+ *                 example: "123456"
+ *     responses:
+ *       200:
+ *         description: Login success
+ *       401:
+ *         description: Invalid credentials
+ */
 
 router.post("/auth/login", authController.login);
 router.get("/auth/me", requireAuth, authController.me);
 
 router.get("/products", requireAuth, productController.list);
-router.get("/dashboard", requireAuth, dashboardController.dashboard);
+router.get("/franchiseStaff_dashboard", requireAuth, franchiseStaff_dashboardController.dashboard);
+
+/**
+ * @swagger
+ * components:
+ *      schema:
+ *          dashboard:
+ *              type: object
+ *              properties:
+ *                  _
+ *          
+ */
+
+/**
+ * @swagger
+ * /api/franchiseStaff_dashboard:
+ * get:
+ *      summary: To come to the dashboard of franchise staff
+ *      description: this api is use to fetch data from database
+ *      response: 
+ *          200:
+ *              description: this api is use to fetch data from database
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: #components/schema/dashboard
+ */
 
 router.post("/orders", requireAuth, orderController.create);
 router.get("/orders", requireAuth, orderController.list);
--- src/server.js
+++ src/server.js
@@ -2,16 +2,40 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const pool = require("./config/database");
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const path = require("path");
+
 
 const apiRoutes = require("./routes/api");
 
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API for Franchise store project',
+            version: '1.0.0'
+        },
+        servers: [
+            {
+                url: 'https://franchisemooncake.onrender.com'
+            }
+        ]
+    },
+    apis: [path.join(__dirname, "./routes/*.js")]
+}
 
 const app = express();
+
+const swaggerSpec = swaggerJSDoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+
 app.use(cors());
 app.use(express.json())
 
 app.use("/api", apiRoutes)
 
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8001;
 app.listen(port, () => console.log("API running on", port));
